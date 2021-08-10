b0ad884931ddd83e1fd31d4c972228bdf8705e55
Andre Silva <andre@particle41.com>
2021-08-10 00:11:47 -0300

    fix tf-module-docs.sh

--- pre_commit_hooks/tf-module-docs.sh
+++ pre_commit_hooks/tf-module-docs.sh
@@ -3,14 +3,19 @@
 ORIGIN="$(pwd)"
 
 tf_docs() {
+  terraform-docs -c "${ORIGIN}/.terraform-docs.yml" .
+}
+
+tf_module() {
   DIR="${1}"
   cd "${DIR}" || exit
-  terraform-docs .
+  tf_docs
 }
 
 for d in "$(pwd)"/modules/*; do
-  tf_docs "${d}"
+  [ -f "README.md" ] && tf_module "${d}"
 done
 
 cd "${ORIGIN}" || exit
+tf_docs
 git add .
